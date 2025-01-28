e88a28c0a26316bc7ad5da0fc177094946286b7e
Siva CS Javvadhi <118528602+siar-cj@users.noreply.github.com>
2025-01-28 10:20:58 +0530

    Update settings.py

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'todoApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('Todos'),
+        'USER': os.getenv('root'),
+        'PASSWORD': os.getenv('Chandu123'),
+        'HOST': os.getenv('mysql-container'), 
+        'PORT': '3306',
     }
 }
 
